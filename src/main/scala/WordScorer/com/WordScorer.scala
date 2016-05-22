91b6f9a052de22f487bbf1a1748cb1267f6c36c8
AndrewArkless <andrewarkless@blueyonder.co.uk>
2016-05-22 12:17:38 +0100

    ScalaTests with dependency injection and start to use Mokito more fully

--- src/main/scala/WordScorer/com/WordScorer.scala
+++ src/main/scala/WordScorer/com/WordScorer.scala
@@ -3,10 +3,11 @@ package WordScorer.com
 /**
   * Created by User on 21/05/2016.
   */
-class WordScorer() {
+class WordScorer(spellChecker:SpellChecker) {
   private val VOWELS=List('a','e','i','o','u')
 
   def score(word:String)={
-    word.foldLeft(0){(total, letter)=> total + (if(VOWELS.contains(letter)) 1 else 2)}
+    if (spellChecker.checkSpelling(word)) word.foldLeft(0){(total, letter)=> total + (if(VOWELS.contains(letter)) 1 else 2)}
+    else 0
   }
 }
--- src/test/scala/WordScorer/com/WordScorerTest.scala
+++ src/test/scala/WordScorer/com/WordScorerTest.scala
@@ -8,24 +8,34 @@ import org.scalatest.mock.MockitoSugar
 /**
   * Created by User on 21/05/2016.
   */
-class WordScorerTest extends FlatSpec with Matchers with MockitoSugar {
+class WordScorerTest extends FlatSpec with Matchers {
 
 
   def withWordScorer(test: WordScorer=>Unit)={
-    val ws=new WordScorer
+    val spellChecker=mock(classOf[SpellChecker])
+    when(spellChecker.checkSpelling(anyString)).thenReturn(true)
+    val ws=new WordScorer(spellChecker)
     test(ws)
   }
 
-  def withNumber(myTest:Int=>Unit)={
-    val b=4
-    myTest(b)
+  def withFalseSpellChecker(test:WordScorer=>Unit)={
+    val falseSpellCheck=mock(classOf[SpellChecker])
+    when(falseSpellCheck.checkSpelling(anyString())).thenReturn(false)
+    val falseWordScorer=new WordScorer(falseSpellCheck)
+    test(falseWordScorer)
+
   }
   "calling word scorer" should "return 2 when ai passed " in {
       withWordScorer(w => w.score("ai") shouldBe 2)
     }
 
-  "call" should "return" in {
-    withNumber(Int=>3 shouldBe 3)
-
+  "calling false spelling word scorer" should "return 0 when spell check returns false" in {
+    withFalseSpellChecker((misspeltWord:WordScorer)=>misspeltWord.score("AndrewB") shouldBe 0)
+  }
+  "calling word scorer" should "return 0 when AndrewB is passed " in {
+    val falseSpellCheck=mock(classOf[SpellChecker])
+    when(falseSpellCheck.checkSpelling(anyString)).thenReturn(false)
+    val wordScorer=new WordScorer(falseSpellCheck)
+    wordScorer.score("AndrewB") shouldBe 0
   }
 }
